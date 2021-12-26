d7dc57abd02ca6e3cd5f8b465b711748388ddc5c
sadahiroyoshi <sadahiroyoshi@gmail.com>
2021-12-26 12:38:10 +0900

    impl ctc loss decode

--- 05ctc/03_decode_ctc.py
+++ 05ctc/03_decode_ctc.py
@@ -4,18 +4,13 @@
 # CTCによるデコーディングを行います
 #
 
-# Pytorchを用いた処理に必要なモジュールをインポート
-import torch
-from torch.utils.data import DataLoader
+import tensorflow as tf
 
 # 作成したDatasetクラスをインポート
 from my_dataset import SequenceDataset
 
 # 数値演算用モジュール(numpy)をインポート
 import numpy as np
-
-# モデルの定義をインポート
-from my_model import MyCTCModel
 
 # json形式の入出力を行うモジュールをインポート
 import json
@@ -25,32 +20,6 @@ import os
 import sys
 
 os.chdir(os.path.dirname(__file__))
-
-def ctc_simple_decode(int_vector, token_list):
-    ''' 以下の手順で，フレーム単位のCTC出力をトークン列に変換する
-        1. 同じ文字が連続して出現する場合は削除
-        2. blank を削除
-    int_vector: フレーム単位のCTC出力(整数値列)
-    token_list: トークンリスト
-    output:     トークン列
-    '''
-    # 出力文字列
-    output = []
-    # 一つ前フレームの文字番号
-    prev_token = -1
-    # フレーム毎の出力文字系列を前から順番にチェックしていく
-    for n in int_vector:
-        if n != prev_token:
-            # 1. 前フレームと同じトークンではない
-            if n != 0:
-                # 2. かつ，blank(番号=0)ではない
-                # --> token_listから対応する文字を抽出し，
-                #     出力文字列に加える
-                output.append(token_list[n])
-            # 前フレームのトークンを更新
-            prev_token = n
-    return output
-
 
 #
 # メイン関数
@@ -88,7 +57,7 @@ if __name__ == "__main__":
     mean_std_file = os.path.join(model_dir, 'mean_std.txt')
 
     # 学習済みのモデルファイル
-    model_file = os.path.join(model_dir, 'best_model.pt')
+    rnn_dir = os.path.join(model_dir, 'best_model')
 
     # デコード結果を出力するディレクトリ
     output_dir = os.path.join(model_dir, 'decode_test')
@@ -106,24 +75,6 @@ if __name__ == "__main__":
     #
     # 設定ここまで
     #
-
-    # 設定ファイルを読み込む
-    with open(config_file, mode='r') as f:
-        config = json.load(f)
-
-    # 読み込んだ設定を反映する
-    # 中間層のレイヤー数
-    num_layers = config['num_layers']
-    # 層ごとのsub sampling設定
-    sub_sample = config['sub_sample']
-    # RNNのタイプ(LSTM or GRU)
-    rnn_type = config['rnn_type']
-    # 中間層の次元数
-    hidden_dim = config['hidden_dim']
-    # Projection層の次元数
-    projection_dim = config['projection_dim']
-    # bidirectional を用いるか(Trueなら用いる)
-    bidirectional = config['bidirectional']
 
     # 出力ディレクトリが存在しない場合は作成する
     os.makedirs(output_dir, exist_ok=True)
@@ -149,8 +100,10 @@ if __name__ == "__main__":
     feat_dim = np.size(feat_mean)
 
     # トークンリストをdictionary型で読み込む
-    # このとき，0番目は blank と定義する
-    token_list = {0: '<blank>'}
+    # decode時にblankは -1 となる
+    # Important: blank labels are returned as -1.
+    # https://docs.w3cub.com/tensorflow~python/tf/keras/backend/ctc_decode
+    token_list = {-1: "<blank>"}
     with open(token_list_path, mode='r') as f:
         # 1行ずつ読み込む
         for line in f: 
@@ -162,109 +115,99 @@ if __name__ == "__main__":
 
     # トークン数(blankを含む)
     num_tokens = len(token_list)
-    
-    # ニューラルネットワークモデルを作成する
-    # 入力の次元数は特徴量の次元数，
-    # 出力の次元数はトークン数となる
-    model = MyCTCModel(dim_in=feat_dim,
-                       dim_enc_hid=hidden_dim,
-                       dim_enc_proj=projection_dim,
-                       dim_out=num_tokens, 
-                       enc_num_layers=num_layers,
-                       enc_bidirectional=bidirectional,
-                       enc_sub_sample=sub_sample,
-                       enc_rnn_type=rnn_type)
-    
-    # モデルのパラメータを読み込む
-    model.load_state_dict(torch.load(model_file))
 
-    # 訓練/開発データのデータセットを作成する
-    test_dataset = SequenceDataset(feat_scp_test,
-                                   label_test,
-                                   feat_mean,
-                                   feat_std)
+    # 以下を踏襲
+    # https://github.com/kutvonenaki/simple_ocr/blob/af05b1697ff4771b611e2fc671bf25f04aa87388/ocr_source/losses.py#L8-L27
+    def custom_ctc():
+        """Custom CTC loss implementation"""
 
-    # 評価データのDataLoaderを呼び出す
-    test_loader = DataLoader(test_dataset,
-                             batch_size=batch_size,
-                             shuffle=False,
-                             num_workers=4)
+        def loss(y_true, y_pred):
+            # print("calc ctc...")
+            # print(y_true)
+            # print(y_pred)
+            """The actual loss"""
+            labels = y_true[:, :, 0]
+            feat_lens = y_true[:, 0, 1]
+            label_lens = y_true[:, 0, 2]
 
-    # CUDAが使える場合はモデルパラメータをGPUに，
-    # そうでなければCPUに配置する
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
-    else:
-        device = torch.device('cpu')
-    model = model.to(device)
+            # reshape for the loss, add that extra dimension
+            feat_lens = tf.expand_dims(feat_lens, -1)
+            label_lens = tf.expand_dims(label_lens, -1)
 
-    # モデルを評価モードに設定する
-    model.eval()
+            # use keras backend function for the loss
+            return tf.keras.backend.ctc_batch_cost(labels, y_pred, feat_lens, label_lens)
+        return loss
+
+    # 学習済みのDNNファイルから
+    # モデルを読み込む
+    model = tf.keras.models.load_model(rnn_dir, custom_objects={"loss": custom_ctc()})
+
+    model.summary()
+
+    # 以下から踏襲
+    # https://github.com/keras-team/keras-io/blob/2f3d2456d894f6669850a4336eed75b717ed0e7e/examples/vision/captcha_ocr.py#L301-L312
+
+    # A utility function to decode the output of the network
+    def decode_batch_predictions(pred):
+        input_len = np.ones(pred.shape[0]) * pred.shape[1]
+        # Use greedy search. For complex tasks, you can use beam search
+        result = tf.keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
+            :, :np.int64(max(input_len))
+        ]
+        return result
+
+    def tokens_to_text(tokens, token_list):
+        # Iterate over the results and get back the text
+        output_text = []
+        for token in tokens:
+            unit = token_list[token]
+            output_text.append(unit)
+        # (' '.join() は，リスト形式のデータを
+        # スペース区切りで文字列に変換している)
+        return ' '.join(output_text)
 
     # デコード結果および正解ラベルをファイルに書き込みながら
     # 以下の処理を行う
     with open(hypothesis_file, mode='w') as hyp_file, \
          open(reference_file, mode='w') as ref_file:
-        # 評価データのDataLoaderから1ミニバッチ
+        # 評価データのfeat_scp_testから1ファイル
         # ずつ取り出して処理する．
-        # これを全ミニバッチ処理が終わるまで繰り返す．
-        # ミニバッチに含まれるデータは，
-        # 音声特徴量，ラベル，フレーム数，
-        # ラベル長，発話ID
-        for (features, labels, feat_lens,
-             label_lens, utt_ids) in test_loader:
 
-            # PackedSequence の仕様上，
-            # ミニバッチがフレーム長の降順で
-            # ソートされている必要があるため，
-            # ソートを実行する
-            sorted_lens, indices = \
-                torch.sort(feat_lens.view(-1),
-                           dim=0,
-                           descending=True)
-            features = features[indices]
-            feat_lens = sorted_lens
+        # 特徴量リスト，ラベルを1行ずつ
+        # 読み込みながら情報を取得する
+        with open(feat_scp_test, mode='r') as file_f, \
+             open(label_test, mode='r') as file_l:
+            for (line_feats, line_label) in zip(file_f, file_l):
+                # 各行をスペースで区切り，
+                # リスト型の変数にする
+                parts_feats = line_feats.split()
+                parts_label = line_label.split()
 
-            # CUDAが使える場合はデータをGPUに，
-            # そうでなければCPUに配置する
-            features = features.to(device)
+                utt_id = parts_feats[0]
+                utt_id_label = parts_label[0]
+                feat_path = parts_feats[1]
+                label = np.int64(parts_label[1:])
 
-            # モデルの出力を計算(フォワード処理)
-            # out_lensは処理後のフレーム数．
-            # sub_sampleを行った場合は，
-            # out_lensはfeat_lensより小さい値になる
-            outputs, out_lens = model(features, feat_lens)
+                # 発話ID(partsの0番目の要素)が特徴量と
+                # ラベルで一致していなければエラー
+                if utt_id != utt_id_label:
+                    sys.stderr.write('IDs of feat and '\
+                        'label do not match.\n')
+                    exit(1)
 
-            # バッチ内の1発話ごとに以下の処理を行う
-            for n in range(outputs.size(0)):
-                # 出力はフレーム長でソートされている
-                # 元のデータ並びに戻すため，
-                # 対応する要素番号を取得する
-                idx = torch.nonzero(indices==n, 
-                                    as_tuple=False).view(-1)[0]
+                feat = np.fromfile(feat_path, 
+                                   dtype=np.float32)
+                # フレーム数 x 次元数の配列に変形
+                feat = feat.reshape(-1, feat_dim)
 
-                # 本来のCTCの確率計算は，
-                # 複数存在するパスを考慮するが，
-                # ここでは簡単のため，各フレームのmax値を
-                # たどる Best path decoding を行う
-                _, hyp_per_frame = torch.max(outputs[idx], 1)
-                # numpy.array型に変換
-                hyp_per_frame = hyp_per_frame.cpu().numpy()
-                # 認識結果の文字列を取得
-                hypothesis = \
-                    ctc_simple_decode(hyp_per_frame,
-                                      token_list)
-             
-                # 正解の文字列を取得
-                reference = []
-                for m in labels[n][:label_lens[n]].cpu().numpy():
-                    reference.append(token_list[m])
-             
+                # モデルの出力を計算(フォワード処理)
+                # 1発話のみのバッチとして扱う
+                preds = model.predict(np.array([feat]))
+                pred_tokens = decode_batch_predictions(preds)
+                pred_tokens = pred_tokens[0].numpy()
+
                 # 結果を書き込む
-                # (' '.join() は，リスト形式のデータを
-                # スペース区切りで文字列に変換している)
                 hyp_file.write('%s %s\n' \
-                    % (utt_ids[n], ' '.join(hypothesis)))
+                    % (utt_id, tokens_to_text(pred_tokens, token_list)))
                 ref_file.write('%s %s\n' \
-                    % (utt_ids[n], ' '.join(reference)))
-
+                    % (utt_id, tokens_to_text(label, token_list)))
